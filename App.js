74408258d75a0195b504c432a4aed448375d1273
kuma88 <kumarasond@gmail.com>
2021-01-26 14:41:43 +0800

    added stack navigator

--- App.js
+++ App.js
@@ -1,27 +1,56 @@
 import React from "react";
 import {View, Text, StyleSheet, TouchableOpacity} from "react-native";
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
 
+function HomeScreen({navigation}) {
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity 
+        style={styles.button} 
+        onPress={() => navigation.navigate('Details')}
+        >
+        <Text style={styles.buttonText}>Add Name</Text>
+      </TouchableOpacity>
+      
+      <TouchableOpacity style={styles.button}>
+      <Text style={styles.buttonText}>Add Expense</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
 
-export default class App extends React.Component{
+function DetailsScreen({navigation}) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Details Screen</Text>
+    </View>
+  );
+}
 
-  render() {
+function AnotherScreen({navigation}) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Another Screen</Text>
+    </View>
+  );
+}
+
+const Stack = createStackNavigator();
+
+
+export default function App() {
 
     return (
-
-      <View style={styles.container}>
-        <TouchableOpacity style={styles.button}>
-          <Text style={styles.buttonText}>Add Name</Text>
-        </TouchableOpacity>
-        
-        <TouchableOpacity style={styles.button}>
-        <Text style={styles.buttonText}>Add Expense</Text>
-        </TouchableOpacity>
-        
-      </View>
-
-    )
-  }
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="HomeScreen" component={HomeScreen} />
+          <Stack.Screen name="Details" component={DetailsScreen} />
+          <Stack.Screen name="Another Screen" component={AnotherScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
 }
 
 const styles = StyleSheet.create({
