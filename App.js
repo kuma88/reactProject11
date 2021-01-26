c594d01e3ef6d91d40733f17c82ef72bd0db4b17
kuma88 <kumarasond@gmail.com>
2021-01-26 11:38:46 +0800

    added two buttons

--- App.js
+++ App.js
@@ -1,21 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import {View, Text, StyleSheet, TouchableOpacity} from "react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+
+export default class App extends React.Component{
+
+  render() {
+
+    return (
+
+      <View style={styles.container}>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Add Name</Text>
+        </TouchableOpacity>
+        
+        <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonText}>Add Expense</Text>
+        </TouchableOpacity>
+        
+      </View>
+
+    )
+  }
 }
 
 const styles = StyleSheet.create({
+
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center", 
+    alignItems: "center",
+    flexDirection: "row",
   },
+
+  buttonText: {
+    textAlign: "center",
+    color: "white",
+  },
+
+  button: {
+    padding: 10,
+    backgroundColor: "blue",
+    borderRadius: 5,
+    margin: 10,
+    marginTop: 30,
+    width: 150,
+  }
+
 });
